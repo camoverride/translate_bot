efff291178725355decf883fe656bae71afb2983
cam <cam@cams-MacBook-Pro.local>
2023-12-24 00:42:40 -0800

    silent mode

--- translate_bot.py
+++ translate_bot.py
@@ -19,46 +19,51 @@ ACTIVE_MODE = True
 
 while True:
     logging.debug("-------------------------------------------")
-    with sr.Microphone() as source:
-        text = None
+    try: # The main loop always continues!
+        with sr.Microphone() as source:
+            text = None
 
-        try:
-            # Get Audio clip
-            logging.debug("> Getting audio")
-            audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
-
-            # Speech-to-text
-            logging.debug("> Speech to text")
             try:
-                text = recognizer.recognize_google(audio, language="en-us")
-            except sr.exceptions.UnknownValueError as e:
-                logging.warning("ERROR recognizing speech. See below:")
+                # Get Audio clip
+                logging.debug("> Getting audio")
+                audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
+
+                # Speech-to-text
+                logging.debug("> Speech to text")
+                try:
+                    text = recognizer.recognize_google(audio, language="en-us")
+                except sr.exceptions.UnknownValueError as e:
+                    logging.warning("ERROR recognizing speech. See below:")
+                    logging.warning(e)
+                    pass
+
+            except sr.exceptions.WaitTimeoutError as e:
+                logging.warning("ERROR getting audio. See below:")
                 logging.warning(e)
                 pass
 
-        except sr.exceptions.WaitTimeoutError as e:
-            logging.warning("ERROR getting audio. See below:")
-            logging.warning(e)
-            pass
+            if text:
+                logging.debug(f"Recognized text input: {text}")
 
-        if text:
-            logging.debug(f"Recognized text input: {text}")
+                new_lang = check_for_translation_change(text)
+                if new_lang:
+                    TRANSLATE_TO = new_lang
+                    logging.debug(f"Switched to {new_lang}")
+                
+                if text == "silent" or "Silent" or "stop" or "Stop":
+                    ACTIVE_MODE = False
+                if text == "active" or "Active" or "start" or "Start":
+                    ACTIVE_MODE = True
+                
+                if ACTIVE_MODE:
+                    translation = translator.translate(text, dest=TRANSLATE_TO).text
+                    logging.debug(f"Translated text: {translation}")
 
-            new_lang = check_for_translation_change(text)
-            if new_lang:
-                TRANSLATE_TO = new_lang
-                logging.debug(f"Switched to {new_lang}")
-            
-            if text == "silent" or "Silent" or "stop" or "Stop":
-                ACTIVE_MODE = False
-            if text == "active" or "Active" or "start" or "Start":
-                ACTIVE_MODE = True
-            
-            if ACTIVE_MODE:
-                translation = translator.translate(text, dest=TRANSLATE_TO).text
-                logging.debug(f"Translated text: {translation}")
+                    logging.debug("Translating text!")
+                    tts = gTTS(translation, lang=TRANSLATE_TO)
+                    tts.save(".tts.mp3")
+                    playsound(".tts.mp3")
 
-                logging.debug("Translating text!")
-                tts = gTTS(translation, lang=TRANSLATE_TO)
-                tts.save(".tts.mp3")
-                playsound(".tts.mp3")
+    except Exception as e:
+        logging.warn(e)
+        pass
