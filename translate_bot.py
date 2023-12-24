8a5177dd25a5eaea74b52533ba37abf2babf9fdd
cam <cam@cams-MacBook-Pro.local>
2023-12-24 01:18:51 -0800

    fiz logic bug

--- translate_bot.py
+++ translate_bot.py
@@ -17,10 +17,36 @@ TRANSLATE_TO = "es"
 ACTIVE_MODE = True
 
 
+
+from ctypes import *
+from contextlib import contextmanager
+import pyaudio
+
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
+
+
+
+
+
+
+
 while True:
     logging.debug("-------------------------------------------")
     try: # The main loop always continues!
-        with sr.Microphone() as source:
+        with noalsaerr() as _, sr.Microphone() as source:
             text = None
 
             try:
