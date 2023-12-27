019900c7d8c2d39d0bef794f1f2da73c8cbee949
cam <cam@cams-MacBook-Pro.local>
2023-12-27 01:26:13 -0800

    bug

--- translate_bot.py
+++ translate_bot.py
@@ -45,53 +45,51 @@ def noalsaerr():
 while True:
     logging.warning(f"Function started at: {datetime.now()}")
     logging.warning(f"Speaking mode on:    {ACTIVE_MODE}")
-    try: # The main loop always continues!
-        with noalsaerr() as _, sr.Microphone() as source:
-            text = None
+    # try: # The main loop always continues!
+    with noalsaerr() as _, sr.Microphone() as source:
+        text = None
 
+        try:
+            # Get Audio clip
+            logging.warning("> Getting audio")
+            audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
+
+            # Speech-to-text
+            logging.warning("> Speech to text")
             try:
-                # Get Audio clip
-                logging.warning("> Getting audio")
-                audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
-
-                # Speech-to-text
-                logging.warning("> Speech to text")
-                try:
-                    text = recognizer.recognize_google(audio, language="en-us")
-                except sr.exceptions.UnknownValueError as e:
-                    logging.warning("ERROR: Speech not understood")
-                    pass
-
-            except sr.exceptions.WaitTimeoutError as e:
-                logging.warning("No speech recorded")
+                text = recognizer.recognize_google(audio, language="en-us")
+            except sr.exceptions.UnknownValueError as e:
+                logging.warning("ERROR: Speech not understood")
                 pass
 
-            if text:
-                logging.warning(f"Recognized text input:  {text}")
+        except sr.exceptions.WaitTimeoutError as e:
+            logging.warning("No speech recorded")
+            pass
 
-                new_lang = check_for_translation_change(text)
-                if new_lang:
-                    TRANSLATE_TO = new_lang
-                    logging.warning(f"Switched to {new_lang}")
+        if text:
+            logging.warning(f"Recognized text input:  {text}")
+
+            new_lang = check_for_translation_change(text)
+            if new_lang:
+                TRANSLATE_TO = new_lang
+                logging.warning(f"Switched to {new_lang}")
+            
+            if (text == "silent") or (text == "stop"):
+                ACTIVE_MODE = False
+            if (text == "active") or (text == "start"):
+                ACTIVE_MODE = True
+            
+            if ACTIVE_MODE:
+                translation = translator.translate(text, dest=TRANSLATE_TO).text
+                logging.warning(f"Translated text output: {translation}")
                 
-                if (text == "silent") or (text == "stop"):
-                    ACTIVE_MODE = False
-                if (text == "active") or (text == "start"):
-                    ACTIVE_MODE = True
-                
-                if ACTIVE_MODE:
-                    # translation = translator.translate(text, dest=TRANSLATE_TO).text
-                    # logging.warning(f"Translated text output: {translation}")
-                    
-                    # Print text
-                    os.system("sudo chmod 777 /dev/usb/lp0")
-                    os.system(f'echo -e "{text}\\n\\n\\n" > /dev/usb/lp0')
-                    # os.system(f"{translation}")
+                # Print text and translation
+                os.system("sudo chmod 777 /dev/usb/lp0")
+                os.system(f'echo "{text}\\n\\n\\n" > /dev/usb/lp0')
+                os.system(f'echo "{translation}\\n\\n\\n" > /dev/usb/lp0')
 
-                    # Print translation
-
-    except Exception as e:
-        logging.warning(e)
-        pass
+    # except Exception as e:
+    #     logging.warning(e)
+    #     pass
 
     logging.warning("--------------------------------------")
